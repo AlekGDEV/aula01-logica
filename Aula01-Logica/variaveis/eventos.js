388b08372d3f096f915749c6736580a9d340ace0
Alexandre Garcia <113113263+AlekGDEV@users.noreply.github.com>
2022-09-13 11:30:52 -0300

    Add files via upload

---
+++ Aula01-Logica/variaveis/eventos.js
@@ -0,0 +1,11 @@
+let numero = 10;
+numero = '20';
+numero = 20 + numero
+
+let nome_completo ='Alexandre';
+nome_completo = nome_completo + ' Garcia'
+
+const SENHA = '123qwe123'
+
+document.write(numero);
+document.write('<br>' + nome_completo)
---
+++ Aula01-Logica/variaveis/variaveis.js
@@ -0,0 +1,7 @@
+let nome = 'Chiquim da Silva';
+let email = 'Chiquim@Chiquim.eu';
+let data_nasc = '01/01/2001';
+let genero = 'Prefiro não informar';
+let matriculado = !true; 
+let endereco = 'Rua do Chiquim, 432';
+let saldo_bancario = 10.00;
